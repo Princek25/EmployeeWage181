c622f22c639eae112a8ff8347f166ad5fbb1eb00
Prince <princek2530@gmail.com>
2022-08-04 11:33:50 +0530

    Added: Daily wage

--- src/com/bridgelabz/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWages.java
@@ -1,15 +1,26 @@
 package com.bridgelabz;
 
 public class EmployeeWages {
+
+    static final int WAGE_PRE_HOUR = 20;
+
+    static final int FULL_DAY_HOURS = 8;
+
+
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program on Main Branch");
         int empPresent = (int)Math.floor(Math.random()*10)%2;
-        System.out.println(empPresent);
+        int empWage = 0;
+
         if(empPresent == 1){
             System.out.println("Employee is Present");
+            empWage = WAGE_PRE_HOUR * FULL_DAY_HOURS;
         }
-        else
+        else {
             System.out.println("Employee is Absent");
-
+        }
+        System.out.println("EmployeeWage = " + empWage);
     }
 }
